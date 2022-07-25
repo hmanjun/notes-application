e136bf2bff34956c62a3b04bcdcb2d34bfb3f0fd
hmanjun <harshith.hm03@gmail.com>
2022-07-25 09:26:06 -0700

    Add listener for post request to api and update db with req

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,27 @@
 const notes = require('express').Router()
 const fs = require('fs')
+const { v4: uuidv4 } = require('uuid');
 const notesData = require('../db/db.json')
 
 notes.get('/', (req,res) => {
     res.json(notesData)
+})
+
+notes.post('/', (req,res) => {
+    const {title,text} = req.body
+    if(title && text){
+        const newNote = {
+            id: uuidv4,
+            title,
+            text
+        }
+        notesData.push(newNote)
+        fs.writeFile('./db/db.json',JSON.stringify(notesData), (err) => {
+            if(err) console.log(err)
+            else res.json(newNote)
+        })
+
+    }
 })
 
 module.exports = notes
