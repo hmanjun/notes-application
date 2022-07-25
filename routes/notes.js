6c6ef129b50eddd04646f5b3b9744b2d26514690
hmanjun <harshith.hm03@gmail.com>
2022-07-25 00:04:56 -0700

    Add middleware

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,9 @@
 const notes = require('express').Router()
 const fs = require('fs')
+const notesData = require('../db/db.json')
 
 notes.get('/', (req,res) => {
-    
+    res.json(notesData)
 })
 
 module.exports = notes
