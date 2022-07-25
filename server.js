936cd85401287daabf68aff7a7ad00520cca5a7b
hmanjun <harshith.hm03@gmail.com>
2022-07-24 23:42:04 -0700

    Add route for homepage and notes get

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 const PORT = 3001
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-    res.sendFile('./public/index.html')
+    res.sendFile(path.join(__dirname,'./public/index.html'))
+})
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
 app.listen(PORT, () =>{
